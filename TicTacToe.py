1c6a64590134acb3494f207b3a81ae3a0a579bd1
Aatman <slrmclaren18@gmail.com>
2018-03-04 02:17:04 -0800

    Functioning two player tic tac toe

--- TicTacToe.py
+++ TicTacToe.py
@@ -5,11 +5,14 @@
 '''
 from tkinter import *
 import tkinter.messagebox
+import copy
 
 root = Tk()
 root.title("Tic Tac Toe")
 
 click = True
+
+
 
 def checker(buttons):
     global click
@@ -42,31 +45,31 @@ def checker(buttons):
 
 buttons=StringVar()
 
-button1 = Button(root, text=" ", font = 'Times 26 bold', height = 4, width = 8, command = lambda:checker(button1))
+button1 = Button(root, text=" ", font = 'Arial 26 bold', height = 4, width = 8, command = lambda:checker(button1))
 button1.grid(row=1, column=0, sticky = S+N+E+W)
 
-button2 = Button(root, text=" ", font = 'Times 26 bold', height = 4, width = 8, command = lambda:checker(button2))
+button2 = Button(root, text=" ", font = 'Arial 26 bold', height = 4, width = 8, command = lambda:checker(button2))
 button2.grid(row=1, column=1, sticky = S+N+E+W)
 
-button3 = Button(root, text=" ", font = 'Times 26 bold', height = 4, width = 8, command = lambda:checker(button3))
+button3 = Button(root, text=" ", font = 'Arial 26 bold', height = 4, width = 8, command = lambda:checker(button3))
 button3.grid(row=1, column=2, sticky = S+N+E+W)
 
-button4 = Button(root, text=" ", font = 'Times 26 bold', height = 4, width = 8, command = lambda:checker(button4))
+button4 = Button(root, text=" ", font = 'Arial 26 bold', height = 4, width = 8, command = lambda:checker(button4))
 button4.grid(row=2, column=0, sticky = S+N+E+W)
 
-button5 = Button(root, text=" ", font = 'Times 26 bold', height = 4, width = 8, command = lambda:checker(button5))
+button5 = Button(root, text=" ", font = 'Arial 26 bold', height = 4, width = 8, command = lambda:checker(button5))
 button5.grid(row=2, column=1, sticky = S+N+E+W)
 
-button6 = Button(root, text=" ", font = 'Times 26 bold', height = 4, width = 8, command = lambda:checker(button6))
+button6 = Button(root, text=" ", font = 'Arial 26 bold', height = 4, width = 8, command = lambda:checker(button6))
 button6.grid(row=2, column=2, sticky = S+N+E+W)
 
-button7 = Button(root, text=" ", font = 'Times 26 bold', height = 4, width = 8, command = lambda:checker(button7))
+button7 = Button(root, text=" ", font = 'Arial 26 bold', height = 4, width = 8, command = lambda:checker(button7))
 button7.grid(row=3, column=0, sticky = S+N+E+W)
 
-button8 = Button(root, text=" ", font = 'Times 26 bold', height = 4, width = 8, command = lambda:checker(button8))
+button8 = Button(root, text=" ", font = 'Arial 26 bold', height = 4, width = 8, command = lambda:checker(button8))
 button8.grid(row=3, column=1, sticky = S+N+E+W)
 
-button9 = Button(root, text=" ", font = 'Times 26 bold', height = 4, width = 8, command = lambda:checker(button9))
+button9 = Button(root, text=" ", font = 'Arial 26 bold', height = 4, width = 8, command = lambda:checker(button9))
 button9.grid(row=3, column=2, sticky = S+N+E+W)
 
 '''
